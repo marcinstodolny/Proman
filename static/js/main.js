fd358a5f3e9ed0ba9582adea959b89bc6d92cd72
dapie346 <dawid.pietruszewski@gmail.com>
2023-02-09 13:14:52 +0100

    renamed files

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,6 @@
 import {boardsManager, reloadBoardsAndCards} from "./controller/boardsManager.js";
 import {initModals} from "./controller/usersManager.js";
-import{webSocket} from "./websocket.js";
+import{webSocket} from "./webSocket.js";
 
 async function init() {
     await boardsManager.loadBoards().then(boardsManager.modifyingColumns);
